0f34a094b5e642a59b7a8401ae8d9944c05b48e0
Jeremy Levett <jezlevett@gmail.com>
2018-02-07 14:58:44 +1100

    made changes, yet to review against standards

--- designs.js
+++ designs.js
@@ -1,10 +1,59 @@
-// Select color input
-// Select size input
+//Elements
+const colorPicker =  document.querySelector('#colorButton');
+const heightBox = document.querySelector('#inputHeight');
+const widthBox =  document.querySelector('#inputWidth');
+const submitBox = document.querySelector('#submitButton');
+const tableArea=document.querySelector('table');
 
-// When size is submitted by the user, call makeGrid()
+//Global variables: [Set to defaults]
+let colorSelected = '#000000'; 
+let height = 1;
+let width = 1;
 
-function makeGrid() {
+//Event Listeners
+colorPicker.addEventListener('change', watchColorPicker, false);
+heightBox.addEventListener('input', heightEntered,false);
+widthBox.addEventListener('input', widthEntered,false);
+submitBox.addEventListener('click',submitPressed,false);
+tableArea.addEventListener('click',cellChangeColor,false);
 
-// Your code goes here!
+//Color input
+function watchColorPicker (color){
+	colorSelected=color.target.value;
+};
+//Height input
+function heightEntered(input){
+	height = input.target.value;
+};
+//Width input
+function widthEntered(input){
+	width = input.target.value;
+};
 
+//'Submit' Button input
+function submitPressed(){
+	while (tableArea.firstChild) 
+    	tableArea.removeChild(tableArea.firstChild);
+	tableArea.insertAdjacentHTML('afterbegin',makeHTMLTable());
+	event.preventDefault();//Needed to add in to prevent form reseting.
 }
+
+function makeHTMLTable(){
+	//TO DO: Try ammending a string or something
+	let returnString='';
+	for(var i=0;i<height;i++){
+		returnString+='<tr>';
+		for(var j=0;j<width;j++)
+			returnString+='<td></td>';	
+		returnString+='</tr>';
+	}
+	return returnString;
+}
+
+//Changes the cell color
+function cellChangeColor(event){
+	if(event.path[0].nodeName == "TD")
+			event.path[0].style.backgroundColor = colorSelected;
+}
+
+
